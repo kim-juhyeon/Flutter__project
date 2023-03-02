eab42a065e368b35366245bf801df5bc23df24fd
juu <totkfka4@gmail.com>
2023-03-03 02:11:02 +0900

    first commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page!"),
+      ),
+      body: Center(
+        child: Text(
+          "환영합니다!",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
